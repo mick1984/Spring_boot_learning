a30849bbd43d4cac18682360dbaadd365845561e
mick1984 <mick_000@sina.com>
2019-10-31 21:45:20 +0800

    mick
    mick

---
+++ spring_boot_acgtale/readme.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+spring boot 搭建完成的一个hello world
+=======
+spring boot 搭建完成的一个acgtale
+>>>>>>> f4ae5656cbfec7a022e24acc19bcf77cd2150c2c
---
+++ spring_boot_acgtale/src/main/java/com/example/SpringAcgTaleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcgTaleApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringAcgTaleApplication.class, args);
+	}
+}
